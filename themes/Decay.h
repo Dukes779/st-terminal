1baaf64484235455e0d0f5026720f325c737817a
Dukes77 <dunkenvizlo@gmail.com>
2022-11-02 18:30:01 +0200

    first

---
+++ themes/Decay.h
@@ -0,0 +1,27 @@
+static const char *colorname[] = {
+      /* 8 normal colors */
+      "#151720",
+      "#dd6777",
+      "#90ceaa",
+      "#ecd3a0",
+      "#86aaec",
+      "#c296eb",
+      "#93cee9",
+      "#cbced3",
+      "#1c1e27",
+
+     /* 8  bright colors */
+      "#e26c7c",
+      "#95d3af",
+      "#f1d8a5",
+      "#8baff1",
+      "#c79bf0",
+      "#98d3ee",
+      "#d0d3d8",
+
+    [256] = "#a5b6cf",
+    [257] = "#0d0f18",
+    [258] = "#a5b6cf",
+
+
+};
