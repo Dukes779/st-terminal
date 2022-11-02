31e626a0f0e05cb2d77028d20bb6c255a3a2b785
Dukes77 <dunkenvizlo@gmail.com>
2022-11-02 19:04:46 +0200

    final

---
+++ themes/gruvbox.h
@@ -0,0 +1,26 @@
+static const char *colorname[] = {
+          /* 8 normal colors */
+                     "#282828",
+                     "#a91e18",
+                     "#888717",
+                     "#c58c1e",
+                     "#386d6f",
+                     "#8f4f6c",
+                     "#578459",
+                     "#8f8270",
+                     "#817466",
+
+             /* 8 bright colors */
+                     "#da3f2d",
+                     "#b8bb26",
+                     "#dba629",
+                     "#658075",
+                     "#bf798c",
+                     "#739b64",
+                      "#baad8d",
+
+            [256] = "#cbbd9a", /* default foreground colour */
+          	[257] = "#272727", /* default background colour */
+        	[258] = "#cbbd9a", /*575268*/
+
+};
---
+++ themes/rose.h
@@ -0,0 +1,24 @@
+static const char *colorname[] = {
+
+               "#1f1d2e",
+               "#eb6f92",
+               "#f6c177",
+               "#ebbcba",
+               "#31748f",
+               "#9ccfd8",
+               "#c4a7e7",
+               "#524f67",
+
+               "#403d52",
+               "#eb6f92",
+               "#f6c177",
+               "#ebbcba",
+               "#31748f",
+               "#9ccfd8",
+               "#c4a7e7",
+               "#e0def4",
+
+         [256] = "#e0def4", /* default foreground colour */
+	     [257] = "#191724", /* default background colour */
+	     [258] = "#e0def4", /*575268*/
+};
